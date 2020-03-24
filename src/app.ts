0c70181c77e6d5f61cf117e5bd9feb8ae482f3d2
David <dalau6@gmail.com>
2020-03-23 22:44:12 -0700

    Fully functioning REST API that handles CRUD.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
 import { connect } from "./db/db";
+import { Movie } from "./db/models/Movie.model";
 
 connect();
 
@@ -15,4 +16,72 @@ app.use(bodyParser.json({
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+
+// Creation
+app.post('/movies', async (req, res) => {
+  const movie = new Movie();
+  movie.title = req.body.title;
+  movie.plot_summary = req.body.plot_summary;
+  movie.duration = req.body.duration;
+  await movie.save();
+  res.send(movie);
+});
+
+// Read
+app.get('/movies', async (req, res) => {
+  const movies = await Movie.find();
+  res.send(movies);
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const movie = await Movie.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).send({ message: "Movie not found" })
+  }
+});
+
+// update
+app.put('/movies/:id', async (req, res) => {
+  const movie = await Movie.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (movie) {
+    if (req.body.title) {
+      movie.title = req.body.title;
+    }
+    if (req.body.plot_summary) {
+      movie.plot_summary = req.body.plot_summary;
+    }
+    if (req.body.duration) {
+      movie.duration = req.body.duration;
+    }
+    await movie.save();
+    res.send(movie);
+  } else {
+    res.status(404).send({ message: "Movie not found" })
+  }
+});
+
+// delete
+app.delete('/movies/:id', async (req, res) => {
+  const movie = await Movie.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (movie) {
+    await movie.remove();
+    res.json({ message: 'Movie deleted' });
+  } else {
+    res.status(404).send({ message: "Movie not found" })
+  }
+});
 export { app };
